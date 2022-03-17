1e10917d417e82c7846eb339c8fe98d93cf72b83
AviH0 <45289949+AviH0@users.noreply.github.com>
2022-03-18 00:52:42 +0200

    add __init__.py to define package

---
+++ new_bci_framework/__init__.py
@@ -0,0 +1,11 @@
+from new_bci_framework.classifier.base_classifier import BaseClassifier
+from new_bci_framework.config.config import Config
+from new_bci_framework.paradigm.paradigm import Paradigm
+from new_bci_framework.paradigm.p300_paradigm import P300Paradaigm
+from new_bci_framework.preprocessing.preprocessing_pipeline import PreprocessingPipeline
+from new_bci_framework.recorder.recorder import Recorder
+from new_bci_framework.recorder.opeb_bci_cyton_recorder import CytonRecorder
+from new_bci_framework.session.session import Session
+from new_bci_framework.session.offline_session import OfflineSession
+
+__all__ = ["BaseClassifier", "Config", "Paradigm", "PreprocessingPipeline", "Recorder", "Session", "OfflineSession"]
