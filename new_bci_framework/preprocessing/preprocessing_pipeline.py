fb21ea5f4d31b5fd4ceb796b52f95e6e7e089b24
AviH0 <45289949+AviH0@users.noreply.github.com>
2022-03-21 13:40:37 +0200

    Add basic preprocessing_pipeline.py

--- new_bci_framework/preprocessing/preprocessing_pipeline.py
+++ new_bci_framework/preprocessing/preprocessing_pipeline.py
@@ -12,7 +12,27 @@ class PreprocessingPipeline:
     """
 
     def __init__(self, config: Config):
-        pass
+        self.__config = config
+        self.__data_dir = self.__config.SESSION_SAVE_DIR
+
+    def __segement(self, data: mne.io.Raw) -> mne.Epochs:
+        event_dict = {v: k for k, v in self.__config.TRIAL_LABELS.items()}
+        events = mne.find_events(data, output="onset", shortest_event=0)
+
+        epochs = mne.Epochs(data,
+                            events,
+                            tmin=self.__config.TRIAL_START_TIME,
+                            tmax=self.__config.TRIAL_END_TIME,
+                            event_id=event_dict,
+                            verbose='INFO',
+                            on_missing='warn')
+        return epochs
+
+    def __filter(self,  data: mne.io.Raw) -> None:
+        data.filter(l_freq=self.__config.LOW_PASS_FILTER, h_freq=self.__config.HIGH_PASS_FILTER)
+        if self.__config.NOTCH_FILTER:
+            data.notch_filter(self.__config.NOTCH_FILTER)
 
     def run_pipeline(self, data: mne.io.Raw) -> mne.Epochs:
-        pass
+        self.__filter(data)
+        return self.__segement(data)
